66854ac204681db5617a986a0632478651e9634c
PC_main <mfranjic995@gmail.com>
2021-02-10 20:19:50 +0100

    Options start

---
+++ Assets/Scripts/OptionsScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsScript : MonoBehaviour
+{
+    public int sliderEnabledHeight;
+    public int sliderDisabledHeight;
+    
+    public void EnableOptions()
+    {
+        RectTransform slider = transform.GetChild(1).gameObject.GetComponent<RectTransform>();
+        Vector2 sizeDelta = new Vector2(slider.sizeDelta.x, sliderEnabledHeight);       
+        Vector3 position = new Vector3(0, 20, 0);
+
+        slider.sizeDelta = sizeDelta;
+        slider.anchoredPosition = position;
+
+        transform.GetChild(1).gameObject.GetComponent<Slider>().interactable = true;
+    }
+
+    public void DisableOptions()
+    {
+        RectTransform slider = transform.GetChild(1).gameObject.GetComponent<RectTransform>();
+        Vector2 sizeDelta = new Vector2(slider.sizeDelta.x, sliderDisabledHeight);
+        Vector3 position = new Vector3(0, 10, 0);
+
+        slider.sizeDelta = sizeDelta;
+        slider.anchoredPosition = position;
+
+        transform.GetChild(1).gameObject.GetComponent<Slider>().interactable = false;
+    }
+}
